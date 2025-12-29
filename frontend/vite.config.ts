f4267da76438dfc197add5319bf09abfa209dcde
Justin-Carl <Justin-Carl@users.noreply.github.com>
2025-12-29 18:48:22 +0800

    rename to OR Reader

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/OR-Generator/",
+  base: "/OR-Reader/",
    plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
